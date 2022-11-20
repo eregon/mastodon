4e4b72a1ffa18de8280a0625e322f9c55df114d1
Matthew Ford <matt@bitzesty.com>
2022-11-20 21:57:24 +0000

    Skip GC disable on truffleruby

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,14 @@
-GC.disable
+unless RUBY_ENGINE == "truffleruby"
+  GC.disable
+end
 
-if ENV['DISABLE_SIMPLECOV'] != 'true'
-  require 'simplecov'
-  SimpleCov.start 'rails' do
-    add_group 'Services', 'app/services'
-    add_group 'Presenters', 'app/presenters'
-    add_group 'Validators', 'app/validators'
+if ENV["DISABLE_SIMPLECOV"] != "true"
+  require "simplecov"
+
+  SimpleCov.start("rails") do
+    add_group("Services", "app/services")
+    add_group("Presenters", "app/presenters")
+    add_group("Validators", "app/validators")
   end
 end
 
@@ -13,11 +16,11 @@ gc_counter = -1
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = "tmp/rspec/examples.txt"
-  config.expect_with :rspec do |expectations|
+  config.expect_with(:rspec) do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  config.mock_with :rspec do |mocks|
+  config.mock_with(:rspec) do |mocks|
     mocks.verify_partial_doubles = true
 
     config.around(:example, :without_verify_partial_doubles) do |example|
@@ -27,17 +30,18 @@ RSpec.configure do |config|
     end
   end
 
-  config.before :suite do
+  config.before(:suite) do
     Rails.application.load_seed
     Chewy.strategy(:bypass)
   end
 
-  config.after :suite do
+  config.after(:suite) do
     gc_counter = 0
     FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
   end
 
-  config.after :each do
+  config.after(:each) do
+    return unless RUBY_ENGINE == "truffleruby"
     gc_counter += 1
 
     if gc_counter > 19
@@ -59,8 +63,14 @@ def json_str_to_hash(str)
 end
 
 def expect_push_bulk_to_match(klass, matcher)
-  expect(Sidekiq::Client).to receive(:push_bulk).with(hash_including({
-    "class" => klass,
-    "args" => matcher
-  }))
+  expect(Sidekiq::Client).to(
+    receive(:push_bulk).with(
+      hash_including(
+        {
+          "class" => klass,
+          "args" => matcher
+        }
+      )
+    )
+  )
 end
